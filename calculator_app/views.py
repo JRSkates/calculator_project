f9c9c37cd3a0c734e4a7e09268024a04166ddc61
JRSkates <skatesproduction@gmail.com>
2023-11-23 08:54:46 +0000

    view created

--- calculator_app/views.py
+++ calculator_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .calculator import Calculator
+
+def home(request):
+  return render(request, 'calculator.app/home.html')
+
+def calculator(request):
+  expression = request.GET.get('expression', '')
+  calc = Calculator()
+
+  try:
+    result = eval(expression, {}, {'calc': calc})
+    calc.result = result
+  except Exception as e:
+    result = f"Error: {str(e)}"
+
+  return JsonResponse({'result': result})
 
 # Create your views here.
