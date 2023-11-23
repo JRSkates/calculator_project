652d85bfdc7efb9f1d9d3361605a1fb64c943a9d
JRSkates <skatesproduction@gmail.com>
2023-11-23 08:50:48 +0000

    calculator class logic added, tests added and adjusted for Django

---
+++ calculator_app/calculator.py
@@ -0,0 +1,100 @@
+import math
+import re
+
+class Calculator:
+  def __init__(self):
+    self.result = 0
+    self.memory = 0
+
+  def add(self, x, y):
+    self.result = x + y
+    return self.result
+  
+  def subtract(self, x, y):
+    self.result = x - y
+    return self.result
+  
+  def multiply(self, x, y):
+    self.result = x * y
+    return self.result
+  
+  def divide(self, x, y):
+    if y == 0:
+      raise ValueError("Division by zero is not allowed")
+    self.result = x / y
+    return self.result
+  
+  def square_root(self, x):
+    if x < 0:
+      raise ValueError("Square root of a negative number is not allowed.")
+    self.result = math.sqrt(x)
+    return self.result
+  
+  def sin(self, x):
+    self.result = round(math.sin(math.radians(x)), 1)
+    return self.result
+  
+  def cos(self, x):
+    self.result = round(math.cos(math.radians(x)), 1)
+    return self.result
+  
+  def tan(self, x):
+    self.result = round(math.tan(math.radians(x)), 1)
+    return self.result
+  
+  def calculate_expression(self, expression):
+    allowed_chars = "0123456789+-*/(). "
+    if any(char not in allowed_chars for char in expression):
+      raise ValueError("Invalid characters in the expression.")
+    
+    if expression.count("(") != expression.count(")"):
+      raise ValueError("Unbalanced parentheses in the expression.")
+    
+    expression = expression.replace("^", "**")
+
+    self.result = eval(expression)
+    return self.result
+
+  def raise_to_power_of(self, x, y):
+    self.result = x**y
+    return self.result
+  
+  def remainder(self, x, y):
+    self.result = x % y
+    return self.result
+  
+  def factorial(self, x):
+    if x < 0:
+      raise ValueError("Factorial is not defined for negative numbers.")
+    self.result = math.factorial(x)
+    return self.result
+  
+  def ln(self, x):
+    if x <= 0:
+      raise ValueError("Natural logarithm is not defined for non-positive numbers.")
+    self.result = math.log(x)
+    return self.result
+  
+  def log(self, x, base):
+    if x <= 0 or base <= 0 or base == 1:
+      raise ValueError("Logarithm is not defined for non-positive numbers or base 1.")
+    self.result = math.log(x, base)
+    return self.result
+  
+  def pi(self):
+    self.result = math.pi
+    return self.result
+  
+  def e(self):
+    self.result = math.e
+    return self.result
+
+  def clear_memory(self):
+    self.memory = 0
+  
+  def store_to_memory(self):
+    self.memory = self.result
+
+  def recall_from_memory(self):
+    self.result = self.memory
+    return self.result
--- calculator_app/tests.py
+++ calculator_app/tests.py
@@ -1,3 +1,88 @@
 from django.test import TestCase
+from calculator_app.calculator import Calculator
+
+class CalculatorTestCase(TestCase):
+  def setUp(self):
+    self.calc = Calculator()
+
+  def test_addition(self):
+    result = self.calc.add(3, 5)
+    assert result == 8
+  
+  def test_subtraction(self):
+    result = self.calc.subtract(5, 3)
+    assert result == 2
+  
+  def test_multiply(self):
+    result = self.calc.multiply(3, 5)
+    assert result == 15
+  
+  def test_divide(self):
+    result = self.calc.divide(10, 2) 
+    assert result == 5
+  
+  def test_raise_to_power_of(self):
+    result = self.calc.raise_to_power_of(2, 4)
+    assert result == 16
+  
+  def test_remainder(self):
+    result = self.calc.remainder(2, 5)
+    assert result == 2
+  
+  def test_square_root(self):
+    result = self.calc.square_root(25)
+    assert result == 5
+  
+  def test_square_root_of_negative_number(self):
+    try:
+      self.calc.square_root(-4)
+    except ValueError as e:
+      assert str(e) == "Square root of a negative number is not allowed."
+  
+  def test_memory_operations(self):
+    self.calc.add(10, 5)
+    self.calc.store_to_memory()
+    self.calc.subtract(8, 3)
+    self.calc.recall_from_memory()
+    assert self.calc.result == 15
+  
+  def test_trigonometric_functions(self):
+    self.calc.sin(30)
+    assert self.calc.result == 0.5
+    self.calc.cos(60)
+    assert self.calc.result == 0.5
+    self.calc.tan(45)
+    assert self.calc.result == 1
+  
+  def test_calculate_expression(self):
+    result = self.calc.calculate_expression("(5 + 3) * 2 - 4 / 2")
+    assert result == 14
+  
+  def test_memory_clear(self):
+    self.calc.add(10, 5)
+    self.calc.store_to_memory()
+    self.calc.clear_memory()
+    self.calc.recall_from_memory()
+    assert self.calc.result == 0
+  
+  def test_factorial(self):
+    self.calc.factorial(5)
+    assert self.calc.result == 120
+  
+  def test_ln(self):
+    self.calc.ln(2.71828)
+    assert round(self.calc.result, 5) == 1
+  
+  def test_log(self):
+    self.calc.log(100, 10)
+    assert round(self.calc.result, 5) == 2
+  
+  def test_pi(self):
+    self.calc.pi()
+    assert round(self.calc.result, 5) == 3.14159
+  
+  def test_e(self):
+    self.calc.e()
+    assert round(self.calc.result, 5) == 2.71828
 
 # Create your tests here.
